c7ab7aac4edb4067199ec6d1c8d3c0b44a154e12
Andy Jones <andytjones@icloud.com>
2019-10-07 19:36:42 +0100

    Changed code to be interactive and allow input of names.

--- directory.rb
+++ directory.rb
@@ -13,6 +13,23 @@ students = [
   {name: "Norman Bates", cohort: :november}
 ]
 #define methods
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  #create a blank new array
+  students = []
+  #gets first name
+  name = gets.chomp
+  #while name is not empty repeat code below
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    #get a new name
+    name = gets.chomp
+  end
+  #return the array of students
+  students
+end
 def print_header  
   puts "The students of Villains Academy"
   puts "-------------"
@@ -25,6 +42,7 @@ end
 def print_footer(names)
   puts "Overall, we have #{names.count} great students."
 end
+students = input_students
 print_header
 print(students)
 print_footer(students)
