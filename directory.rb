c90970c770c8c139418eb281a4cece9df15b720c
Andy Jones <andytjones@icloud.com>
2019-10-07 19:38:54 +0100

    Removed array of names. Was left in in previous commit.

--- directory.rb
+++ directory.rb
@@ -1,17 +1,3 @@
-#lets put the students into an array
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffery Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
 #define methods
 def input_students
   puts "Please enter the names of the students"
