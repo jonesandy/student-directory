19e90f61ea683697a5f302b1d2fd735d9cce294d
Andy Jones <andytjones@icloud.com>
2019-10-08 22:36:35 +0100

    Refactored and interactive menu added.

--- directory.rb
+++ directory.rb
@@ -1,34 +1,65 @@
+@students = []
+
 #define methods
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  #create a blank new array
-  students = []
   #gets first name
   name = gets.chomp
   #while name is not empty repeat code below
   while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     #get a new name
     name = gets.chomp
   end
-  #return the array of students
-  students
 end
+
 def print_header  
   puts "The students of Villains Academy"
   puts "-------------"
 end
-def print(names)
-  names.each do |student|
+
+def print_students_list
+  @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students."
+
+def print_footer
+  puts "Overall, we have #{@students.count} great students."
 end
-students = input_students
-print_header
-print(students)
-print_footer(students)
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection 
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit # this will exit the app
+  else
+    puts "I don't know what you meant. Please try again."
+  end
+end
+
+def interactive_menu
+  loop do   
+    print_menu
+    process(gets.chomp)   
+  end
+end
+
+interactive_menu
