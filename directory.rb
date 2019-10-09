1ce27f11bb89e7767b6e8a570c526e2fe6519533
Andy Jones <andytjones@icloud.com>
2019-10-09 12:34:35 +0100

    Save file code created. Gitignore file created.

--- directory.rb
+++ directory.rb
@@ -33,6 +33,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -48,6 +49,8 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit # this will exit the app
   else
@@ -60,6 +63,16 @@ def interactive_menu
     print_menu
     process(gets.chomp)   
   end
+end
+
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 interactive_menu
