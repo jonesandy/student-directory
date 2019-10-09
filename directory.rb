95a54b48dec7cea44bee4bfdf1253d425cf6c1b5
Andy Jones <andytjones@icloud.com>
2019-10-09 12:55:46 +0100

    Fully functioning file.

--- directory.rb
+++ directory.rb
@@ -5,13 +5,13 @@ def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   #gets first name
-  name = gets.chomp
+  name = STDIN.gets.chomp
   #while name is not empty repeat code below
   while !name.empty? do
     @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
     #get a new name
-    name = gets.chomp
+    name = STDIN.gets.chomp
   end
 end
 
@@ -64,7 +64,7 @@ end
 def interactive_menu
   loop do   
     print_menu
-    process(gets.chomp)   
+    process(STDIN.gets.chomp)   
   end
 end
 
@@ -78,8 +78,8 @@ def save_students
   file.close
 end
 
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(",")
     @students << {name: name, cohort: cohort.to_sym}
@@ -87,4 +87,17 @@ def load_students
   file.close
 end
 
+def try_load_students
+  filename = ARGV.first
+  return if filename.nil? 
+  if File.exists?(filename) 
+    load_students(filename)
+     puts "Loaded #{@students.count} from #{filename}"
+  else
+    puts "Sorry, #{filename} doesn't exist."
+    exit 
+  end
+end
+
+try_load_students
 interactive_menu
